a9a413c76cd77bf8913186f7d0fc264804b5eb81
SeltanRush <akuznetsov.sel@gmail.com>
2022-04-04 23:03:43 +0200

    Added server

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-console.log("Hello node");
+import fastify from "fastify";
+
+const app = fastify({ logger: true });
+
+app.get("/", (req, res) => {
+  res.send("Hello web!");
+});
+
+app.listen(3000, function (err, address) {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server is now listening on ${address}`);
+});
