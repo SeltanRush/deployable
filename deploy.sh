13bf9a6d3e711dc4e0c44a4c7034637f9739079f
SeltanRush <akuznetsov.sel@gmail.com>
2022-04-06 00:49:32 +0200

    Trying to fix deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ echo "Deploying application ..."
 sh build.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nmv install default
+nvm install default
 npx prisma migrate deploy
 docker-compose down && docker-compose up -d
